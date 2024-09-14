f51c6990607ee202f9f30f655000a5130ebd32f6
Torsten Brischalle <torsten@brischalle.de>
2024-09-14 12:42:55 +0200

    Canvas scrollbars added

--- MainGui.py
+++ MainGui.py
@@ -6,6 +6,10 @@ import KkndFileCompression as compression
 import KkndFileContainer as container
 import KkndFileMobd as kkndMobd
 
+FileList : list[container.ContainerFile]
+PhotoImg : tk.PhotoImage
+ListboxFiles : tk.Listbox
+
 def SetPixel(img : tk.PhotoImage, x : int, y : int, colorRgb : int) -> None:
     img.put( f"#{colorRgb & 0xFFFFFF:06X}", ( x, y ) )
 
@@ -30,7 +34,7 @@ def CreateFileAnimationsImage(photoImg : tk.PhotoImage, mobdFile : kkndMobd.Mobd
     for animation in mobdFile.AnimationList:
         idx += 1
         print(f"Draw animation {idx}/{len(mobdFile.AnimationList)}")
-        
+
         offX = 0
         imgHeight = 0
         for frame in animation.FrameList:
@@ -60,8 +64,46 @@ def ListBoxFileSelected(event : Any) -> None:
 
     CreateFileAnimationsImage(PhotoImg, mobdFile)
 
+def BuildGui(window : tk.Tk) -> tuple[tk.PhotoImage, tk.Listbox]:
+    WIDTH = 5000
+    HEIGHT = 5000
+
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=1)
+    window.columnconfigure(2, weight=1000)
+    window.columnconfigure(3, weight=1)
+    window.rowconfigure(0, weight=1000)
+    window.rowconfigure(1, weight=1)
+
+    sbListboxV = tk.Scrollbar(window,orient="vertical")
+    sbListboxV.grid(column=0, row=0, sticky="NS")
+
+    listboxFiles = tk.Listbox(window, yscrollcommand = sbListboxV.set)
+    listboxFiles.grid(column=1, row=0, sticky="NSWE")
+    listboxFiles.bind("<<ListboxSelect>>", ListBoxFileSelected)
+
+    sbListboxV.config(command = listboxFiles.yview) # type: ignore
+
+    sbCanvasV = tk.Scrollbar(window,orient="vertical")
+    sbCanvasV.grid(column=3, row=0, sticky="NS")
+
+    sbCanvasH = tk.Scrollbar(window,orient="horizontal")
+    sbCanvasH.grid(column=2, row=1, sticky="WE")
+
+    canvas = tk.Canvas(window, bg="black", xscrollcommand=sbCanvasH.set, yscrollcommand=sbCanvasV.set, scrollregion=(0,0,WIDTH,HEIGHT))
+    
+    photoImg = tk.PhotoImage(width=WIDTH, height=HEIGHT)
+    canvas.create_image((photoImg.width() / 2, photoImg.height() / 2), image=photoImg, state="normal") # type: ignore
+    
+    canvas.grid(column=2, row=0, sticky="NSWE")
+
+    sbCanvasV.config(command=canvas.yview) # type: ignore
+    sbCanvasH.config(command=canvas.xview) # type: ignore
+
+    return photoImg, listboxFiles
+
 def Main() -> None:
-    global FileList, PhotoImg
+    global FileList, PhotoImg, ListboxFiles
 
     containerData, _, _ = compression.UncompressFile("assets/spritesheets/gamesprt.lpk")
     fileTypeList, _ = container.ReadFileTypeList(containerData)
@@ -72,27 +114,10 @@ def Main() -> None:
     FileList = fileTypeList[0].FileList
     
     window = tk.Tk()
-
-    window.columnconfigure(0, weight=1)
-    window.columnconfigure(1, weight=1)
-    window.columnconfigure(2, weight=1000)
-    window.rowconfigure(0, weight=1)
-
-    sb = tk.Scrollbar(window)
-    sb.grid(column=0, row=0, sticky="NS")
-
-    lb = tk.Listbox(window, yscrollcommand = sb.set)
-    lb.grid(column=1, row=0, sticky="NSWE")
-    lb.bind("<<ListboxSelect>>", ListBoxFileSelected)
-    sb.config(command = lb.yview) # type: ignore
-
-    canvas = tk.Canvas(window, bg="black")
-    PhotoImg = tk.PhotoImage(width=1000, height=1000)
-    canvas.create_image((PhotoImg.width() / 2, PhotoImg.height() / 2), image=PhotoImg, state="normal") # type: ignore
-    canvas.grid(column=2, row=0, sticky="NSWE")
+    PhotoImg, ListboxFiles = BuildGui(window)
     
     for file in FileList:
-        lb.insert(tk.END, file.FileName)
+        ListboxFiles.insert(tk.END, file.FileName)
     
     tk.mainloop()
 
